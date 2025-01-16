d54c55a45a6639ad496606fa820c1d52d2cdad65
manishsingh0418 <manishkumar80819@gmail.com>
2025-01-16 12:20:34 +0530

    initial

---
+++ PythonForDataAnalytics/firstprogram.py
@@ -0,0 +1,17 @@
+# # # print("hii")
+# # print("""Hello world
+# # this is my first python program
+# # I hope you all like it """)
+# print("Hello world \nthis is my first python program \ni hope you all like it ")
+
+# variables in python
+# a ="hello"
+# print(a)
+
+#User Input
+# name =input("Enter your name ")
+# print(name)
+# age=int(input("Enter age here "))
+# print(age)
+# exp1= eval(input("Give input"))
+# print(exp1)
