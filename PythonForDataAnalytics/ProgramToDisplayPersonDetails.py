dca3bd716df6866669669d96ce96de9a315bb2c2
manishsingh0418 <manishkumar80819@gmail.com>
2025-01-20 09:18:49 +0530

    Problem Solving

---
+++ PythonForDataAnalytics/ProgramToDisplayPersonDetails.py
@@ -0,0 +1,6 @@
+name = "John"
+age=23
+address ="654 lake street"
+print (name)
+print (age)
+print (address)
---
+++ PythonForDataAnalytics/typecasting.py
@@ -0,0 +1,27 @@
+#Checking the data type of python
+
+# name ="john"
+# age =23
+# print (type(name))
+
+#implicit typecasting
+#
+# a=123
+# b=1.23
+# print(type(a))
+# print(type(b))
+# c=a+b
+# print(c)
+# print (type(c))
+
+#explicit type conversion
+
+a="123"
+b=1.23
+print(type(a))
+print(type(b))
+a=int(a)
+print("after conversion", type(a))
+c=a+b
+print(c)
+print (type(c))
