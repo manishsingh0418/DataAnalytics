3ef5f0f1370738f245e1e5ac4c9793fc814abf82
manishsingh0418 <manishkumar80819@gmail.com>
2025-01-20 12:33:23 +0530

    Conditional statement

---
+++ PythonForDataAnalytics/ConditionalStatement.py
@@ -0,0 +1,29 @@
+marks =87
+
+
+# if marks >=90:
+#     print('you will get a mobile phone')
+# print("Thank you")
+
+# if marks >=90:
+#     print('you will get a mobile phone')
+# else :
+#     print("no phone for 1 week ")
+# print ("thank you ")
+
+# if marks>=90:
+#     print("You can go to a trip")
+# elif marks >=80 and marks <90 :
+#     print("You will get a new phone")
+# elif marks>=70 and marks <80:
+#     print("You will get a new book")
+# else:
+#     print("You will not get your phone")
+
+# if marks>=80:
+#     print("You will get a new phone")
+#     if marks >=95:
+#         print ("You can go to a trip")
+# else:
+#     print("No phone for a month ")
+if marks >=90 : print("you will get a new phone")
