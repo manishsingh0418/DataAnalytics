4c5001a411add124fa70d17dcbe6a928bd4e4ecc
manishsingh0418 <manishkumar80819@gmail.com>
2025-01-20 09:33:40 +0530

    problem solving

---
+++ PythonForDataAnalytics/ConvertFloatIntoInteger.py
@@ -0,0 +1,6 @@
+x=12.6
+print(type(x))
+
+x=int(x)
+print(type(x))
+print(x)
--- PythonForDataAnalytics/ProgramToSwapTwoVariable.cpp.py
+++ PythonForDataAnalytics/ProgramToSwapTwoVariable.cpp.py
@@ -1,6 +1,12 @@
 x=12
 y=13
-temp=x
-x=y
-y=temp
+#method 1
+
+# temp=x
+# x=y
+# y=temp
+# print(x,y)
+
+#method 2
+x,y=y,x
 print(x,y)
