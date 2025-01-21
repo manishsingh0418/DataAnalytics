0302a1fee2f294f90cd10d76b9a2bcad9a819cbf
manishsingh0418 <manishkumar80819@gmail.com>
2025-01-21 12:40:36 +0530

    problem solving

---
+++ PythonForDataAnalytics/StringProblem.py
@@ -0,0 +1,15 @@
+a="Why fit in , when you are born to stand out "
+b=(len(a))
+print(b)
+
+print(a.count("o"))
+
+x=a.lower()
+print(x)
+y=a.upper()
+print(y)
+
+z=a.title()
+print(z)
+
+print(a. find("fit in"))
---
+++ PythonForDataAnalytics/SumOfFirst10OddNumber.py
@@ -0,0 +1,7 @@
+sum=0
+n=0
+while n<=20:
+    if n%2 !=0:
+        sum+=n
+    n+=1
+print("The sum of first 10 Odd number is ", sum)
