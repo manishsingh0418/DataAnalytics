2d99d4cac93206abe90cf2d9419e67897eb2033c
manishsingh0418 <manishkumar80819@gmail.com>
2025-01-21 20:14:46 +0530

    problem solving

---
+++ PythonForDataAnalytics/StringFunction.py
@@ -0,0 +1,13 @@
+a ="hello"
+b="Hello123"
+c="123456"
+d="HELLO"
+e=" "
+f ="Hello 123@"
+g="1.234"
+
+print(a,a.isalnum())
+print(b,b.isalnum())
+print(c,c.isalnum())
+print(d,d.isalnum())
+print(f,f.isalnum())
