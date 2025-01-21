a5918e3e5530a3c8ea961e69d96dcbd7e58709b1
manishsingh0418 <manishkumar80819@gmail.com>
2025-01-21 11:41:47 +0530

    loops , break and continue statement

---
+++ PythonForDataAnalytics/loops.py
@@ -0,0 +1,24 @@
+# For loop
+
+# for i in range (1,6,2):
+#     print (i)
+
+
+# While loop
+
+# n=0
+# while n<=5:
+#     print(n)
+#     n+=1
+#
+# While TRUE
+
+# while True:
+#     print("Hello")
+while True:
+    num1 = int(input('Enter a number here:'))
+    num2 = int(input('Enter a number here:'))
+    print(num1+num2)
+    repeat=input("do you want to stop the program: ")
+    if repeat=="yes":
+        break
