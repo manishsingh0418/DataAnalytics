c34a2c229e1fd730bc97fe7e19a5d15ac18857cb
manishsingh0418 <manishkumar80819@gmail.com>
2025-01-24 16:28:33 +0530

    InBuilt Module

---
+++ PythonForDataAnalytics/InBuiltModule.py
@@ -0,0 +1,40 @@
+#datetime module
+
+# import datetime
+# x= datetime.datetime.now()
+# print(x)
+#
+# y=datetime.datetime(1997,10,14)
+# print(y)
+
+#Random module
+# import random
+#
+# # x=random.randint(1,10)
+# # print(x)
+#
+# l =["Head","Tails"]
+# x=random.choice(l)
+# print(x)
+
+#Maths Module
+
+import math
+x=max(13,67,45)
+print(x)
+y=min(13,67,45)
+print(y)
+
+a= pow(2,4)
+print(a)
+
+b=math.sqrt(49)
+print(b)
+
+c=abs(-12)
+print(c)
+
+k=math.ceil(2.4)
+print(k)
+m=math.floor(2.4)
+print(m)
