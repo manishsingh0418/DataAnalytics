35502bde4d82a970e6ca4a5cd40e5dfeffc35685
manishsingh0418 <manishkumar80819@gmail.com>
2025-01-31 11:58:03 +0530

    Mathematical Operation in Array

---
+++ PythonForDataAnalytics/MathematicalOperationInArray.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+#Addition
+
+# # arr1=np.array([30,40,60,20,10])
+# # arr2= np.array([30,20,10,20,20])
+# arr1=np.array([[30,40],[20,10]])
+# arr2= np.array([[30,20],[10,20]])
+# print(arr1+arr2)
+# print(np.add(arr1,arr2))
+
+#Subtraction
+
+# arr1=np.array([[30,40],[20,10]])
+# arr2= np.array([[30,20],[10,20]])
+# print(arr1-arr2)
+# print(np.subtract(arr1,arr2))
+
+#mULTIPLY
+
+# arr1=np.array([[30,40],[20,10]])
+# arr2= np.array([[30,20],[10,20]])
+# print(arr1*arr2)
+# print(np.multiply(arr1,arr2))
+
+#Divide
+
+# arr1=np.array([[30,40],[20,10]])
+# arr2= np.array([[30,20],[10,20]])
+# print(arr1/arr2)
+# print(np.divide(arr1,arr2))
+
+#Power
+
+arr1=np.array([3,4,2,1])
+arr2=np.array([3])
+print(np.power(arr1,arr2))
+
+#square root
+
+arr1 = np.array([9,16,4,1])
+print(np.sqrt(arr1))
