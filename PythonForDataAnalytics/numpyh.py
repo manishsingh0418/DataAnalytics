755d929fca4d1c7a4472e049f5b59956572c3678
manishsingh0418 <manishkumar80819@gmail.com>
2025-01-31 10:37:14 +0530

    Creating ,indexing,slicing of numpy array  and attribute of array

---
+++ PythonForDataAnalytics/numpyh.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+#Creation
+
+
+# arr1 =np.array([10,20,30,40])
+# arr =np.array([[10,20,30,40],[50,60,70,80],[20,40,60,80]])
+# print(arr)
+# print(type(arr))
+
+
+# #slicing
+#
+# # arr =np.array([10,20,30,40])
+# # print(arr[0:3])
+# # print(arr[0:])
+# # print(arr[:4])
+# arr =np.array([[10,20,30,40],[50,60,70,80],[20,40,60,80]])
+# print(arr[0:2,0:2])
+# print(arr[0:2,0:3])
+# print(arr[0:3,0:2])
+# print(arr[0,1:3])
+# print(arr[1,1:3])
+# print(arr[2,2:4])
+# print(arr[2,2])
+
+#Attributes of array
+
+# arr =np.array([[10,20,30,40],[50,60,70,80],[20,40,60,80]])
+# print(np.shape(arr))
+# print(np.size(arr))
+# print(np.ndim(arr))
+# print(arr.dtype)
