4bc9a643faaba0725827feee5b50ae1915f764c0
manishsingh0418 <manishkumar80819@gmail.com>
2025-02-10 15:05:35 +0530

    tyle and color

---
+++ PythonForDataAnalytics/StyleAndColor.py
@@ -0,0 +1,29 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+# data=sns.load_dataset("exercise")
+# sns.set_style(style="darkgrid")
+# sns.barplot(x="time",y="pulse",data=data)
+# plt.show()
+
+# data=sns.load_dataset("exercise")
+# sns.set_style(style="whitegrid")
+# sns.barplot(x="time",y="pulse",data=data)
+# plt.show()
+
+# data=sns.load_dataset("exercise")
+# sns.set_style(style="ticks")
+# sns.barplot(x="time",y="pulse",data=data)
+# plt.show()
+
+# data=sns.load_dataset("exercise")
+# sns.barplot(x="time",y="pulse",data=data)
+# plt.show()
+
+# data=sns.load_dataset("exercise")
+# sns.palplot(sns.color_palette("viridis"))
+# plt.show()
+
+# data=sns.load_dataset("exercise")
+# sns.palplot(sns.color_palette("spring"))
+# plt.show()
